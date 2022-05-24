05e08f0616e67b66cc4c6edf11f141662610d7eb
Mateus <mateus.pardinho@gmail.com>
2022-05-24 13:27:45 -0300

    Commit inicial front-end
    Commit inicial do projeto sorteador de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
+
+import{ MatButtonModule } from '@angular/material/button';
+import{ MatToolbarModule } from '@angular/material/toolbar';
+import{ MatInputModule } from '@angular/material/input';
+import{ MatTabsModule } from '@angular/material/tabs';
+import{ MatTableModule } from '@angular/material/table';
+import{ MatIconModule } from '@angular/material/icon';
+import{ MatCardModule } from '@angular/material/card';
+import{ MatDialogModule } from '@angular/material/dialog';
+import{ MatPaginatorModule } from '@angular/material/paginator';
+import{ MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonComponent } from './person/person.component';
+import { PersonService } from './person.service';
+import { SorterComponent } from './sorter/sorter.component';
+import { SorterService } from './sorter.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    SorterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    PersonService,
+    SorterService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sorter/sorter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sorter',
+  templateUrl: './sorter.component.html',
+  styleUrls: ['./sorter.component.css']
+})
+export class SorterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
